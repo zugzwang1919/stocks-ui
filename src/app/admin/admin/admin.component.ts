9711be3ba63c900a45777126bbd7b6c34621a9b4
zugzwang1919 <russell.wolfe@gmail.com>
2020-10-18 13:53:28 -0400

    Add busy support to admin

--- src/app/admin/admin/admin.component.ts
+++ src/app/admin/admin/admin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AlertService } from 'src/app/general/alert/alert.service';
+import { BusyService } from 'src/app/general/busy/busy.service';
 import { AdminService } from '../admin.service';
 
 @Component({
@@ -12,13 +13,12 @@ export class AdminComponent implements OnInit {
 
 
   adminGroup: FormGroup;
-  futureUrl: string;
-  hidePassword = true;
-
+  busy = false;
 
   constructor(
     private adminService: AdminService,
     private alertService: AlertService,
+    private busyService: BusyService,
     private formBuilder: FormBuilder
     ) { }
 
@@ -33,32 +33,65 @@ export class AdminComponent implements OnInit {
     // reset any previous alerts
     this.alertService.clear();
 
+    // Indicate that we're busy
+    this.setBusyStatus(true);
     // Call the admin service
     switch (this.adminGroup.get('requestedAction').value) {
       case '1': {
         this.adminService.updateAllStockPrices()
           .subscribe(
-            result  =>  this.alertService.success(result.summary),
-            error => this.alertService.error(error)
+            result  =>  this.handleSuccessfulResponse(result),
+            error => this.handleErrorResponse(error)
           );
         break;
       }
       case '2': {
         this.adminService.updateAllDividends()
           .subscribe(
-            result  =>  this.alertService.success(result.summary),
-            error => this.alertService.error(error)
+            result  =>  this.handleSuccessfulResponse(result),
+            error => this.handleErrorResponse(error)
           );
         break;
       }
       case '3': {
         this.adminService.updateAllStockSplits()
           .subscribe(
-            result  =>  this.alertService.success(result.summary),
-            error => this.alertService.error(error)
+            result  =>  this.handleSuccessfulResponse(result),
+            error => this.handleErrorResponse(error)
           );
         break;
       }
+      default: {
+        // We've got issues if we get here, but at a minimum indicate that we're not busy any more.
+        this.setBusyStatus(false);
+      }
+    }
+  }
+
+
+
+  private handleSuccessfulResponse(result: any) {
+    // Indicate that we're no longer busy
+    this.setBusyStatus(false);
+    // Show the "success" message
+    this.alertService.success(result.summary);
+
+  }
+
+  private handleErrorResponse(error: any) {
+    // Indicate that we're no longer busy
+    this.setBusyStatus(false);
+    // Show the error
+    this.alertService.error(error);
+  }
+
+  private setBusyStatus(requestedSetting: boolean) {
+    this.busy = requestedSetting;
+    if (requestedSetting) {
+      this.busyService.busy(175);
+    }
+    else {
+      this.busyService.finished(175);
     }
   }
 }
