c29598f2b1bd0b6412d1c50cf7e4a9aede40751f
zugzwang1919 <russell.wolfe@gmail.com>
2020-11-26 11:43:12 -0500

    clean up environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   serverName: '3.94.133.94',
-  serverPort: 8081
+  serverPort: 8081,
+  googleClientId: '173419237297-cpt0q9mevdfq6el6r3rge968iaft7l79.apps.googleusercontent.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   serverName: 'localhost',
-  serverPort: 8081
+  serverPort: 8081,
+  googleClientId: '173419237297-cpt0q9mevdfq6el6r3rge968iaft7l79.apps.googleusercontent.com'
 };
 
 /*
