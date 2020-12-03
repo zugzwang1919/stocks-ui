50adbf567b4095677697b8003cfab40d0fd1a918
zugzwang1919 <russell.wolfe@gmail.com>
2020-12-03 00:04:25 -0500

    Add filters to Ticker Transaction List

--- src/app/ticker-transaction/ticker-transactions/ticker-transactions.component.ts
+++ src/app/ticker-transaction/ticker-transactions/ticker-transactions.component.ts
@@ -6,6 +6,12 @@ import { TickerTransactionService } from '../ticker-transaction.service';
 import { AlertService } from 'src/app/general/alert/alert.service';
 import { WolfeGenericListDirective } from '../../wolfe-common/wolfe-generic-list-directive';
 import { WolfeCheckboxInTableService } from 'src/app/wolfe-common/wolfe-checkbox-in-table.service';
+import { Portfolio } from 'src/app/portfolio/portfolio';
+import { Ticker } from 'src/app/ticker/ticker';
+import { MatSelectChange } from '@angular/material/select';
+import { PortfolioService } from 'src/app/portfolio/portfolio.service';
+import { TickerService } from 'src/app/ticker/ticker.service';
+
 
 @Component({
   selector: 'app-ticker-transactions',
@@ -16,9 +22,19 @@ export class TickerTransactionsComponent extends WolfeGenericListDirective<Ticke
 
   displayedColumns: string[] = ['select', 'date', 'portfolioName', 'ticker', 'activity', 'tradeSize', 'amount'];
 
+  ALL_PORTFOLIO: Portfolio = {id: -1, portfolioName: 'ALL'};
+  portfolios: Portfolio[] = [this.ALL_PORTFOLIO];
+  selectedPortfolio = -1;
+
+  ALL_TICKERS: Ticker = {id: -1, ticker: 'ALL', name: '', benchmark: false};
+  tickers: Ticker[] = [this.ALL_TICKERS];
+  selectedTicker = -1;
+
   constructor(
     router: Router,
     alertService: AlertService,
+    private portfolioService: PortfolioService,
+    private tickerService: TickerService,
     tickerTransactionService: TickerTransactionService,
     public wcits: WolfeCheckboxInTableService
   ) {
@@ -28,13 +44,60 @@ export class TickerTransactionsComponent extends WolfeGenericListDirective<Ticke
           '/ticker-transaction');
   }
 
+
+  ngOnInit() {
+    // First let the super class init
+    super.ngOnInit();
+    // Add the portfolios to the Filter drop down
+    this.portfolioService.retrieveAll()
+    .subscribe(
+      foundPortfolios => foundPortfolios.forEach(fp => this.portfolios.push(fp)),
+      error => this.alertService.error(error)
+    );
+    // Add the tickers to the Filter drop down
+    this.tickerService.retrieveAll()
+    .subscribe(
+      foundTickers => foundTickers.forEach(ft => this.tickers.push(ft)),
+      error => this.alertService.error(error)
+    );
+  }
+
+  onFilterChange(ev: MatSelectChange) {
+    console.log('inside onFilterChange()');
+    this.performFilter(this.filterFunction.bind(this));
+    console.log('leaving onFilterChange()');
+  }
+
+
+  filterFunction(flattenedTickerTransaction: any): boolean {
+
+    if (this.selectedPortfolio === -1 && this.selectedTicker === -1 ) {
+      return true;
+    }
+    if (this.selectedPortfolio !== -1 && this.selectedTicker !== -1) {
+      return  flattenedTickerTransaction.portfolioId === this.selectedPortfolio &&
+              flattenedTickerTransaction.tickerId === this.selectedTicker;
+    }
+    if (this.selectedPortfolio !== -1) {
+      return flattenedTickerTransaction.portfolioId === this.selectedPortfolio;
+    }
+    if (this.selectedTicker !== -1 ) {
+      return flattenedTickerTransaction.tickerId === this.selectedTicker;
+    }
+
+    throw Error('Impossible state encountered in filterFunction of ticker-transaction component.');
+
+  }
+
   flattenItemIfNecessary(tt: TickerTransaction): any {
     const flatItem: any = {};
     flatItem.id = tt.id;
     flatItem.createDate = tt.createDate;
     flatItem.updateDate = tt.updateDate;
+    flatItem.portfolioId = tt.portfolio.id;
     flatItem.portfolioName = tt.portfolio.portfolioName;
     flatItem.date = tt.date;
+    flatItem.tickerId = tt.stock.id;
     flatItem.ticker = tt.stock.ticker;
     flatItem.activity = tt.activity;
     flatItem.tradeSize = tt.tradeSize;
--- src/app/wolfe-common/wolfe-generic-list-directive.ts
+++ src/app/wolfe-common/wolfe-generic-list-directive.ts
@@ -12,6 +12,7 @@ import { WolfeTrackedItem } from './wolfe-tracked-item';
 export class WolfeGenericListDirective<T extends WolfeTrackedItem> {
 
     initialData: any[] = [];
+    unFilteredData: any[] = [];
     dataSource = new MatTableDataSource(this.initialData);
     selection = new SelectionModel(true, []);
 
@@ -45,16 +46,32 @@ export class WolfeGenericListDirective<T extends WolfeTrackedItem> {
           .subscribe(
             // If this goes well, update the list of Items (WolfeTrackedItems to be specific)
             items =>  {
+
               // Flatten the data that was returned by the service so that it can be sorted
               // and then associate it with the DataSource
               this.dataSource.data = items.map(this.flattenItemIfNecessary);
-
+              this.unFilteredData = items.map(this.flattenItemIfNecessary);
               // Reset the check boxes
               this.selection = new SelectionModel(true, []);
             },
             // If the retrieval goes poorly, show the error
             error => this.alertService.error(error)
           );
+    }
+
+    /*
+      performFilter() -
+        Some lists may want to allow users to filter results.  As the user changes what they want
+        to see in the subclass, it is the responsibility of the subclass to call this method
+        and this super class will filter results based on the filter function passed in.
+    */
+    performFilter(filterFunction: (i: any) => boolean) {
+      // If we're asked to filter the list of items being displayed, apply the filter
+      // to the full set of data & then stick the results into the dataSource so that they will
+      // be visible
+      this.dataSource.data = this.unFilteredData.filter(filterFunction);
+      // Reset the check boxes
+      this.selection = new SelectionModel(true, []);
     }
 
     deleteSelectedItems() {
