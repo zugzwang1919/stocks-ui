f974ebe5850358fef92e1737762403dc64197c45
zugzwang1919 <russell.wolfe@gmail.com>
2021-04-27 18:48:39 -0400

    Upgrade Server to Spring Boot 2.4.5

--- git-version.js
+++ git-version.js
@@ -7,7 +7,7 @@ const { writeFileSync } = require('fs');
 const gitInfo = gitDescribeSync();
 const buildDate = new Date();
 gitInfo['buildDate'] = buildDate;
-gitInfo['version'] = '21.01';
+gitInfo['version'] = '21.04';
 const versionInfoJson = JSON.stringify(gitInfo, null, 2);
 
 writeFileSync('git-version.json', versionInfoJson);
